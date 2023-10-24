54b02b2a7428bd1b7edca684ca71356e334d6bcf
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-10-24 21:57:36 +0600

    Initialized Project, installed Tailwind, Daisy UI and Redux

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["lofi"],
+  },
+};
